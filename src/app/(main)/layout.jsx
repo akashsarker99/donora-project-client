dfde33202529d77b51bdcb0e7461736160db0d3b
akashsarker99 <akashsarker2003@gmail.com>
2026-06-21 20:17:49 +0600

    basic setup completed and added navbar

---
+++ src/app/(main)/layout.jsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import React from 'react';
+
+const MainLayout = ({children}) => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div>{children}</div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@heroui/react";
+import { LuDroplets } from "react-icons/lu";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 border-b border-gray-100 bg-white/90 backdrop-blur-md">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3 lg:px-8">
+        
+        <Link href="/" className="flex items-center gap-3">
+          <div className="flex h-12 w-12 items-center justify-center rounded-full bg-[#C70000] shadow-md">
+            <LuDroplets className="text-2xl text-white" />
+          </div>
+
+          <h1
+            className="text-xl font-bold md:text-2xl"
+            style={{ fontFamily: "var(--font-playfair)" }}>
+            Donora
+          </h1>
+        </Link>
+
+        <div className="hidden items-center gap-8 lg:flex">
+          <Link
+            href="/"
+            className="rounded-full bg-red-50 px-6 py-2 font-medium text-[#DC2626] transition"
+          >
+            Home
+          </Link>
+
+          <Link
+            href="/donation-requests"
+            className="font-medium text-gray-600 transition hover:text-[#DC2626]"
+          >
+            Donation Requests
+          </Link>
+
+          <Link
+            href="/funding"
+            className="font-medium text-gray-600 transition hover:text-[#DC2626]"
+          >
+            Funding
+          </Link>
+
+          <Link
+            href="/search"
+            className="font-medium text-gray-600 transition hover:text-[#DC2626]"
+          >
+            Search Donors
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Link
+            href="/login"
+            className="hidden font-medium text-gray-700 transition hover:text-[#DC2626] md:block"
+          >
+            Sign In
+          </Link>
+
+          <Button
+            as={Link}
+            href="/register"
+            radius="full"
+            className="bg-[#C70000] px-6 font-semibold text-white hover:bg-[#820202]"
+          >
+            <LuDroplets></LuDroplets>
+            Register
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
